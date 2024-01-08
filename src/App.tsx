beba8f532552283f10cf69999108487f99070ffd
Jhonatan Romero <jhonatansromeroc@gmail.com>
2024-01-08 16:59:12 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <>
+      <Button variant='primary'> CLICK </Button>
+    </>
+  )
+}
+
+export default App
